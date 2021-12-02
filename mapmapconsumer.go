89d7a903f68a3e8890ddcd964a1dc62c2cad47c5
Dan Goodman <danthegoodman@icloud.com>
2021-12-01 22:20:26 -0500

    pull time from ticker

--- mapmapconsumer.go
+++ mapmapconsumer.go
@@ -25,8 +25,8 @@ func (m *MapMapConsumer) Start() {
 	go func() {
 		for {
 			select {
-			case <-m.ticker.C:
-				nowTime := time.Now().UnixMilli()
+			case n := <-m.ticker.C:
+				nowTime := n.UnixMilli()
 				fmt.Println("ticking", m.lastConsume, nowTime, m.MapMap.CalculateBucket(m.lastConsume), m.MapMap.CalculateBucket(nowTime))
 				m.MapMap.ConsumeRange(m.lastConsume, nowTime, m.ConsumerFunc)
 				m.lastConsume = nowTime + m.MapMap.Interval
