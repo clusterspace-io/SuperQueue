526a72409f2a210b966555c0d6063965b0b58ec2
Dan Goodman <danthegoodman@icloud.com>
2021-12-01 21:17:39 -0500

    add ticker

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"SuperQueue/logger"
+	"time"
 )
 
 var (
 	DelayMapMap   *MapMap
 	InFlightItems *map[ItemID]QueueItem
 	Outbox        chan *QueueItem
+	DelayTicker   time.Ticker
 )
 
 func main() {
