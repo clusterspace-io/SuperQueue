23d9b02a238b81ed59b669f649a364c574a3182d
Dan Goodman <danthegoodman@icloud.com>
2021-12-03 13:56:43 -0500

    lots of function returns

--- queueItem.go
+++ queueItem.go
@@ -7,30 +7,31 @@ type QueueItem struct {
 	Body []byte
 }
 
-// Creates a struct and validates the input
-func NewQueueItem(itemID string, body []byte, delayMS *int64) error {
-
-}
-
 // Adds a new queue item to the DB and immediately queues it
-func (i *QueueItem) AddQueueItem() error {
-	// Create a new
+func (i *QueueItem) EnqueueItem() error {
+	// Add item to DB
+	// Add a newly created queue item to the queue
+	return nil
 }
 
 // Adds a new queue item to the DB and delays it
-func (i *QueueItem) AddDelayedQueueItem() error {
-
+func (i *QueueItem) DelayEnqueueItem(delayMS int64) error {
+	// Add item to DB as delayed
+	// Add item to delay mapmap
+	return nil
 }
 
 func (i *QueueItem) DequeueItem() error {
 	// Write inflight to DB
 	// Move to inflight table
 	// Move to mapmap for timeout capture
+	return nil
 }
 
 func (i *QueueItem) AckItem() error {
 	// Write ack to DB
 	// Remove from inflight table
+	return nil
 }
 
 func (i *QueueItem) NackItem() error {
@@ -39,6 +40,7 @@ func (i *QueueItem) NackItem() error {
 	// Remove from old spot in delayed mapmap
 	// Discard if max attempts exceeded
 	// Add to new spot in delayed mapmap
+	return nil
 }
 
 func (i *QueueItem) TimeoutItem() error {
@@ -46,6 +48,7 @@ func (i *QueueItem) TimeoutItem() error {
 	// Remove from inflight table
 	// Discard if max attempts exceeded
 	// Move to delayed mapmap
+	return nil
 }
 
 // Moving from `delayed`, `nacked`, or `timedout` to `queued` (anything in the delayed mapmap)
@@ -61,8 +64,10 @@ func (i *QueueItem) RequeueItem() {
 
 func (i *QueueItem) addItemToItemsTable() error {
 
+	return nil
 }
 
 func (i *QueueItem) discardItem() error {
 
+	return nil
 }
