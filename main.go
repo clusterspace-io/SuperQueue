3fc6932658976aeb68cf7e1d26ee3c8dcdc30c32
Dan Goodman <danthegoodman@icloud.com>
2021-12-18 20:59:58 -0500

    Adding etcd connecting in

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
+	"github.com/etcd-io/etcd/clientv3"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -16,10 +20,14 @@ var (
 )
 
 func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	c2 := make(chan struct{})
+
 	logger.Logger.Logger.SetLevel(logrus.DebugLevel)
 	logger.Info("Starting SuperQueue")
-	partition := os.Getenv("PARTITION")
-	if partition == "" {
+
+	if PARTITION == "" {
 		logger.Error("Failed to provide a partition using the PARTITION env var, exiting")
 		os.Exit(1)
 	}
@@ -29,13 +37,37 @@ func main() {
 		panic(fmt.Sprintf("Failed to calculate int64 for QUEUE_LEN of %s", GetEnvOrFail("QUEUE_LEN")))
 	}
 
-	SQ = NewSuperQueue("test-ns", partition, 5, QueueMaxLen)
+	SQ = NewSuperQueue("test-ns", PARTITION, 5, QueueMaxLen)
 	go func() {
 		StartHTTPServer()
 	}()
 	SQ.DelayConsumer.Start()
 
-	logger.Info("Setting up DB")
+	// If ETCD_HOSTS exists, start reporting for service discovery
+	if ETCD_HOSTS != "" {
+		logger.Debug("Starting etcd based service discovery")
+		hosts := strings.Split(ETCD_HOSTS, ",")
+		logger.Debug("Using hosts: ", hosts)
+		cli, err := clientv3.New(clientv3.Config{
+			Endpoints:   hosts,
+			DialTimeout: 2 * time.Second,
+			DialOptions: []grpc.DialOption{grpc.WithBlock()}, // Need this to actually fail on connect
+		})
+		if err != nil {
+			logger.Error("Failed to connect to etcd!")
+			logger.Error(err)
+			panic(err)
+		} else {
+			logger.Debug("Connected to etcd")
+		}
+		go func() {
+			<-c2
+			logger.Info("Closing service discovery ticker")
+		}()
+		defer cli.Close()
+	}
+
+	logger.Debug("Setting up DB")
 	// err := ConnectToDB(os.Getenv("CONN_STRING"))
 	DBConnectWithoutKeyspace()
 	DBKeyspaceSetup()
@@ -48,11 +80,10 @@ func main() {
 	// if err != nil {
 	// 	panic(err)
 	// }
-	logger.Info("Done setting up db")
+	logger.Debug("Done setting up db")
 
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
+	c2 <- struct{}{}
 	logger.Info("Closing server")
 	Server.Echo.Close()
 }
