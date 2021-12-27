9e71b38d3fb4a91aba734d0160097084f6dd5513
Dan Goodman <danthegoodman@icloud.com>
2021-12-27 15:25:55 -0500

    reformat for ordering

--- logparser.py
+++ logparser.py
@@ -5,27 +5,27 @@ with open('out.log') as f:
 
   buckets = {
     "POSTRecord": {
-      "le3": 0,
-      "le6": 0,
-      "le9": 0,
-      "le15": 0,
-      "le30": 0,
+      "3": 0,
+      "6": 0,
+      "9": 0,
+      "15": 0,
+      "30": 0,
       "inf": 0,
     },
     "GETRecord": {
-      "le3": 0,
-      "le6": 0,
-      "le9": 0,
-      "le15": 0,
-      "le30": 0,
+      "3": 0,
+      "6": 0,
+      "9": 0,
+      "15": 0,
+      "30": 0,
       "inf": 0,
     },
     "Ack": {
-      "le3": 0,
-      "le6": 0,
-      "le9": 0,
-      "le15": 0,
-      "le30": 0,
+      "3": 0,
+      "6": 0,
+      "9": 0,
+      "15": 0,
+      "30": 0,
       "inf": 0,
     },
   }
@@ -36,41 +36,41 @@ with open('out.log') as f:
       l = json.loads(i)
       if l["method"] == "POST" and l["uri"].startswith("/ack"):
         if l["latency"]<=3000000:
-          buckets["Ack"]["le3"] += 1
+          buckets["Ack"]["3"] += 1
         if l["latency"]<=6000000:
-          buckets["Ack"]["le6"] += 1
+          buckets["Ack"]["6"] += 1
         if l["latency"]<=9000000:
-          buckets["Ack"]["le9"] += 1
+          buckets["Ack"]["9"] += 1
         if l["latency"]<=15000000:
-          buckets["Ack"]["le15"] += 1
+          buckets["Ack"]["15"] += 1
         if l["latency"]<=30000000:
-          buckets["Ack"]["le30"] += 1
+          buckets["Ack"]["30"] += 1
         if l["latency"]>30000000:
           buckets["Ack"]["inf"] += 1
       elif l["method"] == "POST" and l["uri"].startswith("/record"):
         if l["latency"]<=3000000:
-          buckets["POSTRecord"]["le3"] += 1
+          buckets["POSTRecord"]["3"] += 1
         if l["latency"]<=6000000:
-          buckets["POSTRecord"]["le6"] += 1
+          buckets["POSTRecord"]["6"] += 1
         if l["latency"]<=9000000:
-          buckets["POSTRecord"]["le9"] += 1
+          buckets["POSTRecord"]["9"] += 1
         if l["latency"]<=15000000:
-          buckets["POSTRecord"]["le15"] += 1
+          buckets["POSTRecord"]["15"] += 1
         if l["latency"]<=30000000:
-          buckets["POSTRecord"]["le30"] += 1
+          buckets["POSTRecord"]["30"] += 1
         if l["latency"]>30000000:
           buckets["POSTRecord"]["inf"] += 1
       elif l["method"] == "GET" and l["uri"].startswith("/record"):
         if l["latency"]<=3000000:
-          buckets["GETRecord"]["le3"] += 1
+          buckets["GETRecord"]["3"] += 1
         if l["latency"]<=6000000:
-          buckets["GETRecord"]["le6"] += 1
+          buckets["GETRecord"]["6"] += 1
         if l["latency"]<=9000000:
-          buckets["GETRecord"]["le9"] += 1
+          buckets["GETRecord"]["9"] += 1
         if l["latency"]<=15000000:
-          buckets["GETRecord"]["le15"] += 1
+          buckets["GETRecord"]["15"] += 1
         if l["latency"]<=30000000:
-          buckets["GETRecord"]["le30"] += 1
+          buckets["GETRecord"]["30"] += 1
         if l["latency"]>30000000:
           buckets["GETRecord"]["inf"] += 1
 
