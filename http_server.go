d775d3db2e36d620e972a49527e64c1622bb9f17
Dan Goodman <danthegoodman@icloud.com>
2021-12-28 15:39:44 -0500

    new logger

--- http_server.go
+++ http_server.go
@@ -46,7 +46,16 @@ func StartHTTPServer() {
 		Echo: echoInstance,
 	}
 	Server.Echo.HideBanner = true
-	Server.Echo.Use(middleware.Logger())
+	// Server.Echo.Use(middleware.Logger())
+	config := middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out},"proto":"${protocol}"}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+	}
+	Server.Echo.Use(middleware.LoggerWithConfig(config))
 	Server.Echo.Validator = &CustomValidator{validator: validator.New()}
 
 	// Count requests
